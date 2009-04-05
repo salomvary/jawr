d03cb1d0b9f975af44a6c0aeaf4a4574d662784a
icefox <icefox@aba45a81-ba41-0410-aed3-8aeacaedec52>
2009-04-05 20:05:59 +0000

    Set jawr java 1.4 compliant
    git-svn-id: https://jawr.dev.java.net/svn/jawr/branches/development@272 aba45a81-ba41-0410-aed3-8aeacaedec52

--- src/main/java/net/jawr/web/resource/bundle/JoinableResourceBundleContent.java
+++ src/main/java/net/jawr/web/resource/bundle/JoinableResourceBundleContent.java
@@ -119,7 +119,6 @@ public class JoinableResourceBundleContent {
 	/* (non-Javadoc)
 	 * @see java.lang.Object#hashCode()
 	 */
-	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
@@ -130,7 +129,6 @@ public class JoinableResourceBundleContent {
 	/* (non-Javadoc)
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
