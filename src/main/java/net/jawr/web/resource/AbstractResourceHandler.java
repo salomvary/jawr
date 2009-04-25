a2262a25f8874be37d6dd220f9bb7d7831db79b3
icefox <icefox@aba45a81-ba41-0410-aed3-8aeacaedec52>
2009-04-25 13:05:04 +0000

    Fix issue with classpath CSS debug content
    git-svn-id: https://jawr.dev.java.net/svn/jawr/branches/development@277 aba45a81-ba41-0410-aed3-8aeacaedec52

--- src/main/java/net/jawr/web/resource/AbstractResourceHandler.java
+++ src/main/java/net/jawr/web/resource/AbstractResourceHandler.java
@@ -25,6 +25,7 @@ import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.zip.GZIPOutputStream;
 
@@ -262,10 +263,11 @@ public abstract class AbstractResourceHandler  implements ResourceHandler{
 		storeBundle(bundleName,bundleContent,true, gzipDirPath);
 		
 		// Store Css classpath debug files
-		Iterator keyIterator = bundleResourcesContent.getCssClasspathDebugContentMap().keySet().iterator();
+		Map cssClasspathDebugContentMap = bundleResourcesContent.getCssClasspathDebugContentMap();
+		Iterator keyIterator = cssClasspathDebugContentMap.keySet().iterator();
 		while(keyIterator.hasNext()){
 			String filePath = (String) keyIterator.next();
-			storeBundle(filePath,bundleContent,false, cssClasspathDirPath);
+			storeBundle(filePath,(String)cssClasspathDebugContentMap.get(filePath),false, cssClasspathDirPath);
 		}
 	}
 	
