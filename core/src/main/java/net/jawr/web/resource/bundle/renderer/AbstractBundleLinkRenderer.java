2d5098fe43b59b34e448bf5d8bee5dc57e511f04
icefox <icefox@aba45a81-ba41-0410-aed3-8aeacaedec52>
2009-09-01 21:19:55 +0000

    Copy Jawr 3.1.1 in the trunk
    git-svn-id: https://jawr.dev.java.net/svn/jawr/trunk jawr@447 aba45a81-ba41-0410-aed3-8aeacaedec52

--- core/src/main/java/net/jawr/web/resource/bundle/renderer/AbstractBundleLinkRenderer.java
+++ core/src/main/java/net/jawr/web/resource/bundle/renderer/AbstractBundleLinkRenderer.java
@@ -16,7 +16,6 @@ package net.jawr.web.resource.bundle.renderer;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.Random;
-import java.util.Set;
 
 import net.jawr.web.context.ThreadLocalJawrContext;
 import net.jawr.web.resource.bundle.JoinableResourceBundle;
@@ -24,6 +23,7 @@ import net.jawr.web.resource.bundle.factory.util.PathNormalizer;
 import net.jawr.web.resource.bundle.generator.dwr.DWRParamWriter;
 import net.jawr.web.resource.bundle.handler.ResourceBundlesHandler;
 import net.jawr.web.resource.bundle.iterator.ResourceBundlePathsIterator;
+import net.jawr.web.servlet.RendererRequestUtils;
 
 /**
  * Abstract base class for implementations of a link renderer.
@@ -111,10 +111,16 @@ public abstract class AbstractBundleLinkRenderer implements BundleRenderer {
 			out.write(sb.toString());
 		}
 
-		// Retrieve the name or names of bundle(s) that belong to/with the requested path.
-		ResourceBundlePathsIterator it = bundler.getBundlePaths(bundle.getId(), new ConditionalCommentRenderer(out), ctx.getVariantKey());
-
-		renderBundleLinks(it, ctx, debugOn, out);
+		// Include the bundle if it has not been included yet
+		if(ctx.getIncludedBundles().add(bundle.getId())){
+			// Retrieve the name or names of bundle(s) that belong to/with the requested path.
+			ResourceBundlePathsIterator it = bundler.getBundlePaths(bundle.getId(), new ConditionalCommentRenderer(out), ctx.getVariantKey());
+			renderBundleLinks(it, ctx, debugOn, out);
+		}else{
+			if (debugOn) {
+				addComment("The bundle '" + bundle.getId() + "' is already included in the page.", out);
+			}
+		}
 		if (debugOn) {
 			addComment("Finished adding members resolved by " + requestedPath, out);
 		}
@@ -139,7 +145,6 @@ public abstract class AbstractBundleLinkRenderer implements BundleRenderer {
 		String contextPath = ctx.getContextPath();
 		boolean useGzip = ctx.isUseGzip();
 		boolean isSslRequest = ctx.isSslRequest();
-		Set includedBundles = ctx.getIncludedBundles();
 		
 		// Add resources to the page as links.
 		while (it.hasNext()) {
@@ -153,12 +158,12 @@ public abstract class AbstractBundleLinkRenderer implements BundleRenderer {
 					if (random < 0)
 						random *= -1;
 					out.write(createBundleLink(resourceName + "?d=" + random, contextPath, isSslRequest));
-				} else if (!debugOn && useGzip)
+				} else if (!debugOn && useGzip){
 					out.write(createGzipBundleLink(resourceName, contextPath, isSslRequest));
-				else
+				}else{
 					out.write(createBundleLink(resourceName, contextPath, isSslRequest));
-					
-				if (!includedBundles.add(resourceName) && debugOn) {
+				}
+				if(!ctx.getIncludedResources().add(resourceName)){
 					addComment("The resource '" + resourceName + "' is already included in the page.", out);
 				}
 			}
@@ -206,45 +211,14 @@ public abstract class AbstractBundleLinkRenderer implements BundleRenderer {
 		}
 		String fullPath = PathNormalizer.joinPaths(bundler.getConfig().getServletMapping(), bundleId);
 
-		String contextPathOverride = getContextPathOverride(isSslRequest);
-		// If the contextPathOverride is not null and we are in production mode,
-		// or if we are in debug mode but we should use the contextPathOverride even in debug mode
-		// then use the contextPathOverride
-		if(contextPathOverride != null && 
-				((bundler.getConfig().isDebugModeOn() && bundler.getConfig().isUseContextPathOverrideInDebugMode()) ||
-				!bundler.getConfig().isDebugModeOn())) {
-			
-				String override = contextPathOverride;
-				// Blank override, create url relative to path
-				if ("".equals(override)) {
-					fullPath = fullPath.substring(1);
-				} else
-					fullPath = PathNormalizer.joinPaths(override, fullPath);
-		} else
-			fullPath = PathNormalizer.joinPaths(contextPath, fullPath);
-
+		fullPath = RendererRequestUtils.getRenderedUrl(fullPath, bundler.getConfig(), contextPath, isSslRequest);
+		
 		// allow debugOverride to pass through on the generated urls
 		if (ThreadLocalJawrContext.isDebugOverriden()) {
 			fullPath = PathNormalizer.addGetParameter(fullPath, "overrideKey", bundler.getConfig().getDebugOverrideKey());
 		}
 
 		return renderLink(fullPath);
-	}
-
-	/**
-	 * Returns the context path depending on the request mode (SSL or not)
-	 * 
-	 * @param isSslRequest teh flag indicating that the request is an SSL request
-	 * @return the context path depending on the request mode
-	 */
-	private String getContextPathOverride(boolean isSslRequest) {
-		String contextPathOverride = null;
-		if (isSslRequest) {
-			contextPathOverride = bundler.getConfig().getContextPathSslOverride();
-		} else {
-			contextPathOverride = bundler.getConfig().getContextPathOverride();
-		}
-		return contextPathOverride;
 	}
 
 	/**
